2ae58889134fd5880bc9181fa65ff198100b4228
MarnickSeidel <marnickseidel@gmail.com>
2020-03-23 15:49:09 +0100

    initial commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:personal_finance/model/Transaction.dart';
+
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tx) {
+        return Card(
+            child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2)),
+              child: Text('\$${tx.amount}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple)),
+              padding: EdgeInsets.all(10),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  tx.title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                Text(
+                  DateFormat.yMMMd().format(tx.date),
+                  style: TextStyle(color: Colors.grey, fontSize: 10),
+                )
+              ],
+            )
+          ],
+        ));
+      }).toList(),
+    );
+  }
+}
