19dc5732fc942e8281724eafea82e29e678b157d
marnickseidel <Marnickseidel@gmail.com>
2020-03-26 13:19:05 +0100

    add automatic closing changing stateles to stateful to save form data when changing

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:personal_finance/widgets/new_transaction.dart';
-import 'package:personal_finance/widgets/transaction_list.dart';
 
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 import 'model/Transaction.dart';
 
-void main() => runApp(MyHomePage());
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> transactions = [
+  final List<Transaction> _userTransactions = [
     Transaction(
-        id: 't1', title: 'new shoes', amount: 20.21, date: DateTime.now()),
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
     Transaction(
-        id: 't2', title: 'groceries', amount: 40.21, date: DateTime.now())
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
@@ -28,21 +49,21 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     setState(() {
-      transactions.add(newTx);
+      _userTransactions.add(newTx);
     });
   }
 
   void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
-        context: ctx,
-        builder: (_)
-    {
-      return GestureDetector(
-        onTap: () {},
-        child: NewTransaction(_addNewTransaction),
-        behavior: HitTestBehavior.opaque,
-      );
-    };
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
   }
 
   @override
@@ -59,18 +80,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: SingleChildScrollView(
         child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Chart'),
-                ),
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
               ),
-              TransactionList(transactions),
-            ]),
+            ),
+            TransactionList(_userTransactions),
+          ],
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
